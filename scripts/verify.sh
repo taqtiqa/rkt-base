e0fcd5f45d03e51638aab229d4e6028600c5811a
Mark Van de Vyver <mark@taqtiqa.com>
2017-07-23 23:30:31 +1000

    Altered licenses added attribution CC-SAv4
    These details were not immediately obvious at first brush.
    
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 #
 # Copyright (C) 2017 TAQTIQA LLC. <http://www.taqtiqa.com>
+# Copyright (C) 2014 Enrico Zimuel
 #
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU Affero General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU Affero General Public License v3
-#along with this program.
-#If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>.
-#
+#The contents of this file are licensed under a Creative Commons
+#Attribution-ShareAlike 4.0 International License.
+#See <http://creativecommons.org/licenses/by-sa/4.0/deed.en_US>
 
 # Verify a file with a public key using OpenSSL
 # Decode the signature from Base64 format
