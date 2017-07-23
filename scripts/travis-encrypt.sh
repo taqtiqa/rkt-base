b01fd897c39635da951ba76a0f6f3d91fccab43f
Mark Van de Vyver <mark@taqtiqa.com>
2017-07-23 23:31:46 +1000

    travis-encrypt using bash only and the public key.
    Eliminates the need to install ruby etc for this functionality.
    
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

---
+++ scripts/travis-encrypt.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2017 TAQTIQA LLC. <http://www.taqtiqa.com>
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License v3
+#along with this program.
+#If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+#
+
+# NOTE:
+# This script relies on the public key for this repo created by Travis.
+# Travis is the CI environment setup to support this encrypted environment variables.
+#
+# Encrypt a string with a public key using OpenSSL
+# Encode the encrypted string in Base64 format
+#
+# Usage: travis-encrypt <public_key> <string>
+#
+#
+
+publickey=$1
+string=$2
+
+if [[ $# -lt 2 ]] ; then
+  echo "Usage: sign <file> <private_key>"
+  exit 1
+fi
+
+SEKRET_ENC=$(echo "${string}" | openssl pkeyutl -encrypt -pubin -inkey $publickey | base64 --wrap 0)
+
+echo "global:" >>./travis-todo.yml
+echo "  - secure: ${SEKRET_ENC}" >>./travis-todo.yml
+
+echo "Raw: ${string}"
+echo "Encrypted: ${SEKRET_ENC}"
